2128a6d7dbef0b97b1acf7767122d12956f1b89e
David Van Duzer <dvd@tennica.net>
2018-02-04 15:30:10 -0700

    generated project structure

---
+++ h3-swift/h3_swift.h
@@ -0,0 +1,19 @@
+//
+//  h3_swift.h
+//  h3-swift
+//
+//  Created by David Van Duzer on 2/4/18.
+//  Copyright © 2018 David Van Duzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for h3_swift.
+FOUNDATION_EXPORT double h3_swiftVersionNumber;
+
+//! Project version string for h3_swift.
+FOUNDATION_EXPORT const unsigned char h3_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <h3_swift/PublicHeader.h>
+
+
